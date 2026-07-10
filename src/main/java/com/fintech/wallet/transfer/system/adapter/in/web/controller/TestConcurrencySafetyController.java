5b3d48c98792dec4065c88c5532d1aaae36e964b
Minh Chau <5751071002@st.utc2.edu.vn>
2026-07-10 15:16:05 +0700

    Implement concurrency safety with virtual threads and update Java version to 21

---
+++ src/main/java/com/fintech/wallet/transfer/system/adapter/in/web/controller/TestConcurrencySafetyController.java
@@ -0,0 +1,61 @@
+package com.fintech.wallet.transfer.system.adapter.in.web.controller;
+
+import com.fintech.wallet.transfer.system.application.port.in.TransferMoneyUseCase;
+import com.fintech.wallet.transfer.system.domain.common.UUIDv7;
+import com.fintech.wallet.transfer.system.domain.model.Transaction;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/test-concurrency-safety")
+public class TestConcurrencySafetyController {
+
+    private final TransferMoneyUseCase transferMoneyUseCase;
+
+    public TestConcurrencySafetyController(TransferMoneyUseCase transferMoneyUseCase) {
+        this.transferMoneyUseCase = transferMoneyUseCase;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> testConcurrencySafety() {
+        int numberOfRequest = 10000;
+        Map<String, Integer> resultMaps = new ConcurrentHashMap<>();
+
+        // test with [VirtualThreadPerTask]
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < numberOfRequest; i++) {
+                executor.execute(() -> {
+                    // Start of the [Task]
+                    String key = UUIDv7.randomUUID().toString();
+                    TransferMoneyUseCase.TransferTransactionCommand command = new TransferMoneyUseCase.TransferTransactionCommand(
+                            key,
+                            "57511783565900642",
+                            "57511783565793344",
+                            BigDecimal.ONE,
+                            "USD",
+                            key
+                    );
+                    try {
+                        Transaction result = transferMoneyUseCase.transfer(command);
+                        resultMaps.merge(result.getStatus().name(), 1, Integer::sum);
+                    } catch (Exception exception) {
+                        resultMaps.merge(exception.getMessage(), 1, Integer::sum);
+                    }
+                    // End of the [Task]
+                });
+            }
+        }
+
+        return ResponseEntity.ok(resultMaps);
+    }
+}
--- src/main/java/com/fintech/wallet/transfer/system/application/service/TransferMoneyService.java
+++ src/main/java/com/fintech/wallet/transfer/system/application/service/TransferMoneyService.java
@@ -15,7 +15,6 @@ import com.fintech.wallet.transfer.system.domain.model.Transaction;
 import com.fintech.wallet.transfer.system.domain.model.Wallet;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Service;
@@ -39,61 +38,67 @@ public class TransferMoneyService implements TransferMoneyUseCase {
     @Override
     @Transactional
     @Retryable(
-            includes = {ObjectOptimisticLockingFailureException.class, OptimisticLockingFailureException.class},
-            maxRetries = 3,
-            multiplier = 2.0
+            includes = {ObjectOptimisticLockingFailureException.class},
+            maxRetries = 10,    // 10 times
+            delay = 50,         // 50ms
+            multiplier = 2.0    // 50ms -> 100ms -> 200ms -> 400ms -> 800ms ...
     )
     public Transaction transfer(TransferTransactionCommand command) {
-        log.info("START: Money transfer processing ...");
+        try {
+            log.info("[START]: Money transfer processing ...");
 
-        /* 1. Check duplicate [Transaction] by [IdempotencyKey] */
-        transactionRepositoryPort.findByIdempotencyKey(command.idempotencyKey()).ifPresent(transaction -> {
-            throw new BusinessException(BusinessError.DUPLICATE_TRANSACTION_BY_KEY, transaction.getIdempotencyKey());
-        });
+            /* 1. Check duplicate [Transaction] by [IdempotencyKey] */
+            transactionRepositoryPort.findByIdempotencyKey(command.idempotencyKey()).ifPresent(transaction -> {
+                throw new BusinessException(BusinessError.DUPLICATE_TRANSACTION_BY_KEY, transaction.getIdempotencyKey());
+            });
 
-        /* 2. Read [Source Wallet] and [Target Wallet] without lock (optimistic lock) */
-        Wallet sourceWallet = walletRepositoryPort.findByWalletNumber(command.sourceWalletNumber())
-                .orElseThrow(() -> new ApplicationException(ApplicationError.WALLET_NOT_FOUND_BY_WALLET_NUMBER, command.sourceWalletNumber()));
-        Wallet targetWallet = walletRepositoryPort.findByWalletNumber(command.targetWalletNumber())
-                .orElseThrow(() -> new ApplicationException(ApplicationError.WALLET_NOT_FOUND_BY_WALLET_NUMBER, command.targetWalletNumber()));
+            /* 2. Read [Source Wallet] and [Target Wallet] without lock (optimistic lock) */
+            Wallet sourceWallet = walletRepositoryPort.findByWalletNumber(command.sourceWalletNumber())
+                    .orElseThrow(() -> new ApplicationException(ApplicationError.WALLET_NOT_FOUND_BY_WALLET_NUMBER, command.sourceWalletNumber()));
+            Wallet targetWallet = walletRepositoryPort.findByWalletNumber(command.targetWalletNumber())
+                    .orElseThrow(() -> new ApplicationException(ApplicationError.WALLET_NOT_FOUND_BY_WALLET_NUMBER, command.targetWalletNumber()));
 
-        /* 3.1 Create [Transaction] for business logic (Status=NEW) */
-        Transaction newTransaction = Transaction.createNew(
-                command.idempotencyKey(),
-                sourceWallet,
-                targetWallet,
-                Money.of(command.amount(), command.currency()),
-                command.description()
-        );
+            /* 3.1 Create [Transaction] for business logic (Status=NEW) */
+            Transaction newTransaction = Transaction.createNew(
+                    command.idempotencyKey(),
+                    sourceWallet,
+                    targetWallet,
+                    Money.of(command.amount(), command.currency()),
+                    command.description()
+            );
 
-        /* 3.2 Mark [Transaction] is started (Status=IN_PROGRESS) */
-        newTransaction.start();
+            /* 3.2 Mark [Transaction] is started (Status=IN_PROGRESS) */
+            newTransaction.start();
 
-        /* 4. Create a [Pair] of [LedgerEntry] for transfer money process (credit/IN and debit/OUT) */
-        DoubleEntryLedger postingData = DoubleEntryLedger.forTransfer(
-                newTransaction.getId(),
-                sourceWallet.getId(),
-                targetWallet.getId(),
-                newTransaction.getAmount()
-        );
+            /* 4. Create a [Pair] of [LedgerEntry] for transfer money process (credit/IN and debit/OUT) */
+            DoubleEntryLedger postingData = DoubleEntryLedger.forTransfer(
+                    newTransaction.getId(),
+                    sourceWallet.getId(),
+                    targetWallet.getId(),
+                    newTransaction.getAmount()
+            );
 
-        /* 5. Do entry for the [Ledger] is first priority (the source of truth) */
-        ledgerEntryRepositoryPort.saveAll(postingData.getEntries());
+            /* 5. Do entry for the [Ledger] is first priority (the source of truth) */
+            ledgerEntryRepositoryPort.saveAll(postingData.getEntries());
 
-        /* 6. Apply change to [Source Wallet] and [Target Wallet] */
-        sourceWallet.debit(newTransaction.getAmount());
-        targetWallet.credit(newTransaction.getAmount());
-        walletRepositoryPort.save(sourceWallet);
-        walletRepositoryPort.save(targetWallet);
+            /* 6. Apply change to [Source Wallet] and [Target Wallet] */
+            sourceWallet.debit(newTransaction.getAmount());
+            targetWallet.credit(newTransaction.getAmount());
+            walletRepositoryPort.save(sourceWallet);
+            walletRepositoryPort.save(targetWallet);
 
-        /* 7. Mark [Transaction] is completed (Status=COMPLETED) */
-        newTransaction.completed();
-        Transaction saved = transactionRepositoryPort.save(newTransaction);
+            /* 7. Mark [Transaction] is completed (Status=COMPLETED) */
+            newTransaction.completed();
+            Transaction saved = transactionRepositoryPort.save(newTransaction);
 
-        /* 8. Save result of [Transaction] as [Cache] */
-        idempotencyKeyPort.saveResult(command.idempotencyKey(), saved);
+            /* 8. Save result of [Transaction] as [Cache] */
+            idempotencyKeyPort.saveResult(command.idempotencyKey(), saved);
 
-        log.info("END: Money transfer processing!");
-        return saved;
+            log.info("[END]: Money transfer processing!");
+            return saved;
+        } catch (Exception exception) {
+            log.error("[ERROR]: {}", exception.getMessage());
+            throw exception;
+        }
     }
 }
